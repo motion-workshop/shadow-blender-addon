a07dc4ae6734ff34df4ec185f70f0ad828edce7a
Luke Tokheim <luke@motionnode.com>
2013-03-19 18:54:25 -0700

    Added MotionSDK sample for the Blender Python console. Control the Motion Service from Blender.

---
+++ example.py
@@ -0,0 +1,28 @@
+#
+# Example SDK usage from the Blender Python console. Control the Motion Service
+# using the socket based Lua console. The SDK wraps the Lua commands in the
+# Node class for convenient Python use.
+#
+
+import Motion.MotionSDK as SDK
+
+# Script node. Use just like the Lua "node" module. Connect to Lua console.
+node = SDK.LuaConsole.Node(SDK.Client("127.0.0.1", 32075))
+
+# Set up the configuration.
+node.close()
+node.open("Shadow.mNode")
+node.start()
+
+# Create rest pose.
+node.set_pose_marker()
+
+# Zero position. Start recording.
+node.set_pose()
+node.start_take()
+
+# ...
+
+# Stop recording. Export to BVH.
+node.stop_take()
+node.export("take.bvh")
